0d6a528a354eb70eef9538669b6d31e1714a8dfe
Christian Jacobsen <chribben@gmail.com>
2016-01-27 17:24:58 +0100

    Fix front

--- src/services/front/FrontController.cs
+++ src/services/front/FrontController.cs
@@ -1,22 +1,22 @@
 using Microsoft.AspNet.Mvc;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+
 namespace WebApi
 {
   [Route("api/[controller]")]
-  public class FrontController
+  public class FrontController : Controller
   {
-    HttpClient _client = null;
-    string _uri;
-    FrontController()
-    {
-        _client = new HttpClient();
-        _uri = "";
-    }
+    HttpClient _client = new HttpClient();
+    string _uri = "http://127.0.0.1:8000/";
+
     [HttpGet]
     public async Task<string> Get()
     {
-        return await _client.GetStringAsync(_uri);
+        var result = await _client.GetStringAsync(_uri);
+        System.Console.WriteLine(result);
+        return result;
     }
   }
 }
--- src/services/front/Startup.cs
+++ src/services/front/Startup.cs
@@ -1,19 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace HelloMvc
+namespace front
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseMvc();
         }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
     }
 }
