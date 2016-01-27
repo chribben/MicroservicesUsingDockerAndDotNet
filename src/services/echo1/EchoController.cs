6af691d68273c50952f1df31530ed3bff84df549
Tomas Lycken <tomas.lycken@gmail.com>
2016-01-27 15:26:29 +0100

    Echo server v1.0

---
+++ src/services/echo1/EchoController.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.WebEncoders;
+using Microsoft.AspNet.Mvc;
+
+namespace Echo.Controllers
+{
+    public class EchoController : Controller
+    {
+        [Route("/"), HttpPost]
+        public IActionResult Echo([FromBody] string input)
+        {
+            return Ok($"Echo server: {input}");
+        }
+    }
+}
